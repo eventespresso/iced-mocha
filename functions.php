04a3ded7316a740c338efeaf5623db92ee6e972e
Seth <seth@eventespresso.com>
2014-06-18 18:24:39 -0600

    Fixing version var

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@
  */
 
 // variable for theme version
-define ("iced_mocha_VERSION","1.0");
+define ("ICED_MOCHA_VERSION","1.0");
 
 require_once(dirname(__FILE__) . "/admin/main.php"); // Load necessary admin files
 
