d121a82879351872e040deb51de3c46ebd8ac1ec
Josh Feck <josh@nxnweb.com>
2017-02-22 11:54:04 -0500

    Add some specific date and time formats

--- content/espresso/content-espresso_events-header.php
+++ content/espresso/content-espresso_events-header.php
@@ -1,4 +1,7 @@
-<?php global $post; ?>
+<?php global $post; 
+$date_format = apply_filters( 'iced_mocha_espresso_events_header_template_date_format', get_option( 'date_format' ) );
+$time_format = apply_filters( 'iced_mocha_espresso_events_header_template_time_format', get_option( 'time_format' ) );
+?>
 <header class="event-header">
 	<h1 id="event-details-h1-<?php echo $post->ID; ?>" class="entry-title">
 		<a class="" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
@@ -7,7 +10,7 @@
 	<p id="event-date-p">
 		<?php 
 		if ( isset( $post->EE_Event ) && $post->EE_Event instanceof EE_Event ) {
-			echo $post->EE_Event->primary_datetime()->start_date_and_time(); 
+			echo $post->EE_Event->primary_datetime()->start_date_and_time( $date_format, $time_format ); 
 		}				
 		?>
 	</p>
